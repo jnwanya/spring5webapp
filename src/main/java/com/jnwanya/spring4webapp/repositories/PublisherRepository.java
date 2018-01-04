182c7c025c01fcf62c37f9be4d06e75999ea9281
Nwanya Justin <nwanyajustin@gmail.com>
2018-01-04 18:06:08 +0100

    Added publisher repository

---
+++ src/main/java/com/jnwanya/spring4webapp/repositories/PublisherRepository.java
@@ -0,0 +1,11 @@
+package com.jnwanya.spring4webapp.repositories;
+
+import com.jnwanya.spring4webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Jnwanya on
+ * Thu, 04 Jan, 2018.
+ */
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+}
