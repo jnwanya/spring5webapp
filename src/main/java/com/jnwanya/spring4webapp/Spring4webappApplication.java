8f5655fb1234d5ebdf67f1293db45f0944ff43e2
Nwanya Justin <nwanyajustin@gmail.com>
2018-01-04 14:28:15 +0100

    Created springboot application

---
+++ src/main/java/com/jnwanya/spring4webapp/Spring4webappApplication.java
@@ -0,0 +1,12 @@
+package com.jnwanya.spring4webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4webappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jnwanya/spring4webapp/Spring4webappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jnwanya.spring4webapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring4webappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
